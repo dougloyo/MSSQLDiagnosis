7904dd2f1128c1d0ea57d0a66aff3d0f408d3602
Douglas Loyo <douglasloyo@gmail.com>
2015-04-25 19:51:36 -0500

    Adding files

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace MSSQLDiagnosis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["Connection"].ConnectionString;
+
+            string queryString = ConfigurationManager.AppSettings["SQLStatement"];
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var command = new SqlCommand(queryString, connection);
+
+                try
+                {
+                    connection.Open();
+                    var count = command.ExecuteScalar();
+                    Console.WriteLine("Everything seems good. Record count for query is {0}", count);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                Console.WriteLine("Click any key to close.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
